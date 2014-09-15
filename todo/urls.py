b051e541d34fd52972f229a49c4d2f88f163f0b0
Asta K <ksawie@gmail.com>
2014-09-15 12:19:35 +0400

    settings5

---
+++ todo/urls.py
@@ -0,0 +1,40 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from rest_framework import routers, serializers, viewsets
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.conf import settings
+from tasks.models import Task
+admin.autodiscover()
+
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = ('title','id')
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+
+urlpatterns = patterns('',
+                       url(r'^$',
+                           TemplateView.as_view(template_name='base.html')),
+                       url(r'^', include(router.urls)),
+                       url(r'^api-auth/', include('rest_framework.urls',
+                                                  namespace='rest_framework')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
+if settings.DEBUG:
+    urlpatterns = patterns('',
+                           url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                               {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+                           url(r'',
+                               include('django.contrib.staticfiles.urls')),
+                           ) + urlpatterns
